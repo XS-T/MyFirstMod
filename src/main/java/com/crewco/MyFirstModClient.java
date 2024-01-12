08d622379e12e9548a2634417aebf72676bda789
XS-T <xstudios383@gmail.com>
2024-01-12 03:04:42 -0600

    IntialCommit

---
+++ src/main/java/com/crewco/MyFirstModClient.java
@@ -0,0 +1,10 @@
+package com.crewco;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyFirstModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
